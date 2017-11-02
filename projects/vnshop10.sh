8c0ea26b5249d34dae0da0f0751f1338f78db39d
Fuyijie <2037016629@qq.com>
2017-11-02 17:57:09 +0800

    add vnshop10.sh

--- projects/vnshop10.sh
+++ projects/vnshop10.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
- 
-WEB_PATH='/home/wwwroot/vnshop/'
-WEB_PATH_CLIENT='/home/wwwroot/vnshop/client'
+  
+WEB_PATH='/home/wwwroot/shangxian/'
+WEB_PATH_CLIENT='/home/wwwroot/shangxian/client'
 WEB_USER='www'
 WEB_USERGROUP='www'
 # we can do 
-echo "Start deployment vx.itnote.cn"
+echo "Start deployment fuyijie.xin"
 cd $WEB_PATH
 echo "pulling source code..."
 # git reset --hard origin/release
